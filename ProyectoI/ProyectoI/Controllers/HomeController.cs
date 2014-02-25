497f39008a675225d261d5b5594c99990ea8ae71
MichaelJimenezHerrera <michaeljimenezh69@gmail.com>
2014-02-25 07:38:23 -0600

    contacto

--- ProyectoI/ProyectoI/Controllers/HomeController.cs
+++ ProyectoI/ProyectoI/Controllers/HomeController.cs
@@ -26,61 +26,48 @@ namespace ProyectoI.Controllers
         [HttpPost]
         public ActionResult Contact(string nombre = "", string email = "", string contraseña = "", string asunto = "", string comentario = "")
         {
-            /*-------------------------MENSAJE DE CORREO----------------------*/
-
-            //Creamos un nuevo Objeto de mensaje
-            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
-
-            //Direccion de correo electronico a la que queremos enviar el mensaje
-            mmsg.To.Add("michaeljimenezh@hotmail.com");
-
-            //Nota: La propiedad To es una colección que permite enviar el mensaje a más de un destinatario
-
-            //Asunto
-            mmsg.Subject = asunto;
+                   //datos del mensaje y el correo al que le vamos a enviar el mensaje
+            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();//Creamos un nuevo Objeto para el mensaje mmsg
+           
+            mmsg.To.Add("michaeljimenezh@hotmail.com");//Direccion de correo electronico a la que queremos enviar el mensaje//con To nos permite enviar mensajes a mas de un correo
+            
+            mmsg.Subject = asunto;//Asunto del correo
             mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
 
-            //Direccion de correo electronico que queremos que reciba una copia del mensaje
-            //mmsg.Bcc.Add("michaeljimenezh@hotmail.com"); //Opcional
-
-            //Cuerpo del Mensaje
-            mmsg.Body = comentario;
+            //mmsg.Bcc.Add("michaeljimenezh@hotmail.com"); para enviar una copia del correo si queremos
+           
+            mmsg.Body = comentario;//El mensaje que vamos a enviar
             mmsg.BodyEncoding = System.Text.Encoding.UTF8;
             mmsg.IsBodyHtml = false; //Si no queremos que se envíe como HTML
+            
+            mmsg.From = new System.Net.Mail.MailAddress(email);//Correo electronico desde la que enviamos el mensaje
 
-            //Correo electronico desde la que enviamos el mensaje
-            mmsg.From = new System.Net.Mail.MailAddress(email);
-
-
-            /*-------------------------CLIENTE DE CORREO----------------------*/
-
-            //Creamos un objeto de cliente de correo
-            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
-
-            //Hay que crear las credenciales del correo emisor
+            //Aqui van los datos del que va a enviar el mensaje
+            
+            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();//Creamos un objeto de cliente de correo
+            
             cliente.Credentials =
-                new System.Net.NetworkCredential(email,contraseña);
+                new System.Net.NetworkCredential(email,contraseña);//Hay que crear las credenciales del correo que envia el mensaje
 
-            //Lo siguiente es obligatorio si enviamos el mensaje desde Gmail
+            
+            cliente.Port = 587;//esto es obligatorio si enviamos el mensaje desde Gmail
+            cliente.EnableSsl = true;//esto es obligatorio si enviamos el mensaje desde Gmail
 
-            cliente.Port = 587;
-            cliente.EnableSsl = true;
 
-
-            cliente.Host = "smtp.gmail.com"; //Para Gmail "smtp.gmail.com";
+            cliente.Host = "smtp.gmail.com"; 
             //cliente.Host = "smtp.hotmail.com"; 
 
-            /*-------------------------ENVIO DE CORREO----------------------*/
+            // y enviamos nuestro correo
 
             try
             {
-                //Enviamos el mensaje      
-                cliente.Send(mmsg);
+                    
+                cliente.Send(mmsg);//Enviamos el mensaje  
             }
             catch (System.Net.Mail.SmtpException ex)
             {
 
-                //Aquí gestionamos los errores al intentar enviar el correo
+               // aqui todos los erros al no poder enviar el correo
             }
             return View();
         }
