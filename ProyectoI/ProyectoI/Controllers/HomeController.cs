51cb9e05f98d940192f447bcde4f418d7610501a
MichaelJimenezHerrera <michaeljimenezh69@gmail.com>
2014-02-25 07:18:56 -0600

    contacto

--- ProyectoI/ProyectoI/Controllers/HomeController.cs
+++ ProyectoI/ProyectoI/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Net.Mail;
 namespace ProyectoI.Controllers
 {
     public class HomeController : Controller
@@ -11,7 +11,7 @@ namespace ProyectoI.Controllers
         //
         // GET: /Home/
 
-        public ActionResult Index()
+        public ActionResult Home()
         {
             return View();
         }
@@ -20,6 +20,74 @@ namespace ProyectoI.Controllers
         {
             return View();
         }
+
+
+
+        [HttpPost]
+        public ActionResult Contact(string nombre = "", string email = "", string contraseña = "", string asunto = "", string comentario = "")
+        {
+            /*-------------------------MENSAJE DE CORREO----------------------*/
+
+            //Creamos un nuevo Objeto de mensaje
+            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+
+            //Direccion de correo electronico a la que queremos enviar el mensaje
+            mmsg.To.Add("michaeljimenezh@hotmail.com");
+
+            //Nota: La propiedad To es una colección que permite enviar el mensaje a más de un destinatario
+
+            //Asunto
+            mmsg.Subject = asunto;
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            //Direccion de correo electronico que queremos que reciba una copia del mensaje
+            //mmsg.Bcc.Add("michaeljimenezh@hotmail.com"); //Opcional
+
+            //Cuerpo del Mensaje
+            mmsg.Body = comentario;
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = false; //Si no queremos que se envíe como HTML
+
+            //Correo electronico desde la que enviamos el mensaje
+            mmsg.From = new System.Net.Mail.MailAddress(email);
+
+
+            /*-------------------------CLIENTE DE CORREO----------------------*/
+
+            //Creamos un objeto de cliente de correo
+            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+
+            //Hay que crear las credenciales del correo emisor
+            cliente.Credentials =
+                new System.Net.NetworkCredential(email,contraseña);
+
+            //Lo siguiente es obligatorio si enviamos el mensaje desde Gmail
+
+            cliente.Port = 587;
+            cliente.EnableSsl = true;
+
+
+            cliente.Host = "smtp.gmail.com"; //Para Gmail "smtp.gmail.com";
+            //cliente.Host = "smtp.hotmail.com"; 
+
+            /*-------------------------ENVIO DE CORREO----------------------*/
+
+            try
+            {
+                //Enviamos el mensaje      
+                cliente.Send(mmsg);
+            }
+            catch (System.Net.Mail.SmtpException ex)
+            {
+
+                //Aquí gestionamos los errores al intentar enviar el correo
+            }
+            return View();
+        }
+
+
+
+
 
         public ActionResult Recommendations()
         {
